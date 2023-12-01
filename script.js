e89583e82d0479bf4bc36d042ba4604b2ea3b35d
michaellhan <michaelhan707@gmail.com>
2023-11-30 22:36:42 -0500

    worok plase

--- script.js
+++ script.js
@@ -4,6 +4,7 @@ let dotsElement;
 let canvasWidth = 720; // Set the width of the canvas
 let canvasHeight = 720; // Set the height of the canvas
 let dots = []; // Array to store the coordinates of the dots
+let selectedDot = null;
 
 function preload() {
   field = loadImage('field.png');
@@ -12,7 +13,6 @@ function preload() {
 function setup() {
   const canvas = createCanvas(canvasWidth, canvasHeight);
   canvas.parent('canvas-container');
-  canvas.elt.oncontextmenu = (e) => e.preventDefault(); // Disable context menu on the canvas
   field.resize(canvasWidth, canvasHeight); // Resize the image to fit the canvas
   coordinatesElement = document.getElementById('coordinates');
   dotsElement = document.getElementById('dots');
@@ -20,9 +20,13 @@ function setup() {
 
 function draw() {
   background(field);
-  fill(255, 0, 0, 100); // Set the color to red with 40% opacity
   for (let dot of dots) {
-    ellipse(dot.x, dot.y, 40); // Make each circle 4 times the size
+    if (dot === selectedDot) {
+      fill(255, 0, 0, 200); // Darken the selected dot
+    } else {
+      fill(255, 0, 0, 100);
+    }
+    ellipse(dot.x, dot.y, 40);
   }
   if (mouseX >= 0 && mouseX <= width && mouseY >= 0 && mouseY <= height) {
     const x = (mouseX - width / 2) / (width / 2) * 72;
@@ -35,21 +39,33 @@ function draw() {
 
 function mouseClicked() {
   if (mouseX >= 0 && mouseX <= width && mouseY >= 0 && mouseY <= height) {
+    for (let dot of dots) {
+      const distance = dist(mouseX, mouseY, dot.x, dot.y);
+      if (distance < 20) {
+        if (dot === selectedDot) {
+          selectedDot = null; // Deselect the dot if it's already selected
+        } else {
+          selectedDot = dot; // Select the dot
+        }
+        return;
+      }
+    }
     if (mouseButton === LEFT) {
       const x = (mouseX - width / 2) / (width / 2) * 72;
       const y = -(mouseY - height / 2) / (height / 2) * 72;
       dots.push({x: mouseX, y: mouseY, displayX: Math.round(x), displayY: Math.round(y)}); // Add the dot to the array
       dotsElement.innerHTML += `<li>(${Math.round(x)}, ${Math.round(y)})</li>`;
-    } else if (mouseButton === RIGHT) {
-      for (let i = dots.length - 1; i >= 0; i--) {
-        const dot = dots[i];
-        const distance = dist(mouseX, mouseY, dot.x, dot.y);
-        if (distance < 40) { // If the click is within the circle
-          dots.splice(i, 1); // Remove the dot from the array
-          dotsElement.removeChild(dotsElement.childNodes[i]);
-          break;
-        }
-      }
+    }
+  }
+}
+
+function keyPressed() {
+  if (keyCode === BACKSPACE && selectedDot) {
+    const index = dots.indexOf(selectedDot);
+    if (index !== -1) {
+      dots.splice(index, 1); // Remove the selected dot from the array
+      dotsElement.removeChild(dotsElement.childNodes[index]);
+      selectedDot = null; // Deselect the dot
     }
   }
 }
