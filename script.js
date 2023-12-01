b0b8dd21d34139a4afa75b9dfc626bf84b93ffda
michaellhan <michaelhan707@gmail.com>
2023-11-30 22:17:06 -0500

    ok

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 let field;
 let coordinatesElement;
+let dotsElement;
 let canvasWidth = 720; // Set the width of the canvas
 let canvasHeight = 720; // Set the height of the canvas
 let dots = []; // Array to store the coordinates of the dots
@@ -11,8 +12,10 @@ function preload() {
 function setup() {
   const canvas = createCanvas(canvasWidth, canvasHeight);
   canvas.parent('canvas-container');
+  canvas.elt.oncontextmenu = (e) => e.preventDefault(); // Disable context menu on the canvas
   field.resize(canvasWidth, canvasHeight); // Resize the image to fit the canvas
   coordinatesElement = document.getElementById('coordinates');
+  dotsElement = document.getElementById('dots');
 }
 
 function draw() {
@@ -21,6 +24,9 @@ function draw() {
   for (let dot of dots) {
     ellipse(dot.x, dot.y, 40); // Make each circle 4 times the size
   }
+  const x = (mouseX - width / 2) / (width / 2) * 72;
+  const y = -(mouseY - height / 2) / (height / 2) * 72;
+  coordinatesElement.textContent = `(${Math.round(x)}, ${Math.round(y)})`;
 }
 
 function mouseClicked() {
@@ -28,14 +34,15 @@ function mouseClicked() {
     if (mouseButton === LEFT) {
       const x = (mouseX - width / 2) / (width / 2) * 72;
       const y = -(mouseY - height / 2) / (height / 2) * 72;
-      dots.push({x: mouseX, y: mouseY}); // Add the dot to the array
-      coordinatesElement.textContent = `(${Math.round(x)}, ${Math.round(y)})`;
+      dots.push({x: mouseX, y: mouseY, displayX: Math.round(x), displayY: Math.round(y)}); // Add the dot to the array
+      dotsElement.innerHTML += `<li>(${Math.round(x)}, ${Math.round(y)})</li>`;
     } else if (mouseButton === RIGHT) {
       for (let i = dots.length - 1; i >= 0; i--) {
         const dot = dots[i];
         const distance = dist(mouseX, mouseY, dot.x, dot.y);
         if (distance < 20) { // If the click is within the circle
           dots.splice(i, 1); // Remove the dot from the array
+          dotsElement.removeChild(dotsElement.childNodes[i]);
           break;
         }
       }
