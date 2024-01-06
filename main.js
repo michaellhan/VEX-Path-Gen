b69d39e6d5c10525cf288de6b65bbfb6d7a1090a
bn615 <chessanole@gmail.com>
2024-01-06 11:06:49 -0500

    Update main.js

--- main.js
+++ main.js
@@ -49,15 +49,14 @@ function positionUI() {
   userDropdown.style.left = formX + 'px';
   userDropdown.style.top = formY + 'px';
 
-  const downloadB = document.getElementById('#download');
-  downloadB.style.display = 'flex'; 
-  downloadB.style.gap = '20px';
+  downloadButton.style.display = 'flex'; 
+  downloadButton.style.gap = '20px';
 
   const downloadX = window.innerWidth * 4.25 / 6; // X position for the user input form 
   const downloadY = 100; // Y position for the user input
-  downloadB.style.position = 'absolute';
-  downloadB.style.left = downloadX + 'px';
-  downloadB.style.top = downloadY + 'px';
+  downloadButton.style.position = 'absolute';
+  downloadButton.style.left = downloadX + 'px';
+  downloadButton.style.top = downloadY + 'px';
 }
 
 
