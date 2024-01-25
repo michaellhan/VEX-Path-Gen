740dfec85844e72a9e278676bd0f093b36fc7457
bn615 <chessanole@gmail.com>
2024-01-24 21:41:32 -0500

    testing

--- main.js
+++ main.js
@@ -235,11 +235,7 @@ function mouseClicked() {
       
       dots.push({x: mouseX, y: mouseY, displayX: Math.round(x), displayY: Math.round(y)}); // Add the dot to the array
       dotsElement.innerHTML += `<li>(${Math.round(x)}, ${Math.round(y)})</li>`;
-      if (selectedMethod === 'cubic-spline') {
-        let mappedDots = dots.map(dot => new Point(dot.x, dot.y));
-        adjustPointForCollinearity(mappedDots, mappedDots.length - 1);
-        dots = mappedDots.map(pt => ({ x: pt.x, y: pt.y }));
-      }
+      
     }
   }
 }
